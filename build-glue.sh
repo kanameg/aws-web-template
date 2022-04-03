ca971ee6200e31658664f54eb82823aa51e76f25
kanameg <kaname.g@gmail.com>
2022-04-04 01:16:02 +0900

    作成と削除用のスクリプトを追加

--- build-glue.sh
+++ build-glue.sh
@@ -3,8 +3,15 @@
 STACKNAME=tempalte-glue
 S3BUCKETNAME=sh10-datalake-bucket
 
+aws s3 cp glue-job.py s3://${S3BUCKETNAME}/scripts/
+
 aws cloudformation create-stack \
 --stack-name ${STACKNAME} \
 --template-body file://glue-stack.yml \
 --parameters ParameterKey=S3BucketName,ParameterValue=${S3BUCKETNAME} \
 --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation wait stack-create-complete \
+--stack-name ${STACKNAME}
+
+echo "Glue 作成完了"
--- delete-glue.sh
+++ delete-glue.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 STACKNAME=tempalte-glue
+S3BUCKETNAME=sh10-datalake-bucket
 
 aws cloudformation delete-stack \
 --stack-name ${STACKNAME}
+
+aws s3 rm s3://${S3BUCKETNAME}/output/parquet/sales/o/ --recursive
+
+aws cloudformation wait stack-delete-complete \
+--stack-name ${STACKNAME}
+
+echo "Glue 削除完了"
