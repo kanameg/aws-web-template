e1516aa98f02735f5c5860e3f0227f11dae3d0d1
kanameg <kaname.g@gmail.com>
2022-04-04 23:49:59 +0900

    エンドポイントの作成位置を変更

--- build-gateway-vpce.sh
+++ build-gateway-vpce.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 VPCID=vpc-0563273dd3c38a4a0
-ROUTETABLEID1=rtb-02713a399b3306f5e
+#ROUTETABLEID1=rtb-02713a399b3306f5e
+ROUTETABLEID1=rtb-0c40deea78844c016
 
 aws cloudformation create-stack \
 --stack-name tempalte-gateway-vpce \
