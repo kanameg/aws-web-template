208f129fcd6bb779f7986377744c54e19e2c7fd0
kanameg <kaname.g@gmail.com>
2022-04-16 16:12:03 +0900

    プライベート2つの構成を追加

---
+++ 2public-subnets/build-2public2private.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ROOTTEMPLATE=./network-2public.yml
+STACKNAME=tempalte-network
+S3URL=s3://template-bucket-template/template/
+
+# テンプレートファイルのコピー
+aws s3 cp network/network-vpc.yml ${S3URL}network/
+aws s3 cp network/network-public-subnet.yml ${S3URL}network/
+aws s3 cp network/network-internet-gateway.yml ${S3URL}network/
+aws s3 cp network/network-nat-gateway.yml ${S3URL}network/
+
+# スタックの作成
+aws cloudformation create-stack \
+--stack-name ${STACKNAME} \
+--template-body file://${ROOTTEMPLATE}
+
+# 作成完了待ち
+aws cloudformation wait stack-create-complete \
+--stack-name ${STACKNAME}
