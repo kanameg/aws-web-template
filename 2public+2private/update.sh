dd195b011a23944d43d64ec79d1ed0f5a755be83
kanameg <kaname.g@gmail.com>
2022-04-16 21:38:41 +0900

    パラメーター引数修正

--- 2public+2private/update.sh
+++ 2public+2private/update.sh
@@ -11,7 +11,7 @@ aws s3 cp ../network ${S3_URL} --recursive
 # スタックの更新
 aws cloudformation update-stack \
 --stack-name ${STACK_NAME} \
---parameters ParameterKey=System, ParameterValue=${SYSTEM} \
+--parameters ParameterKey=System,ParameterValue=${SYSTEM} \
 --template-body file://${ROOT_TEMPLATE}
 
 # 作成完了待ち
